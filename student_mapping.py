3ddcda50b62f3ad3fdf85341f9e88a92b61693ef
stualden <github@stu.aldenmail.com>
2026-04-17 04:59:55 -0400

    Put IDs into student_mapping dictionary

---
+++ student_mapping.py
@@ -0,0 +1,112 @@
+student_mapping = {
+    01: "Tyler Alday",
+    02: "Sean Allen",
+    03: "Jake Barton",
+    04: "Adonai Belete",
+    05: "TJ Brodnik",
+    06: "Maxx Byrne",
+    07: "Rauden Clarke",
+    08: "Brody Collins",
+    09: "Anthony Costa",
+    10: "Gabe Danisi",
+    11: "Braiden DeLacy",
+    12: "Connor Dodson",
+    13: "Jonah Grunes",
+    14: "John Hank",
+    15: "Desmond Heneks",
+    16: "Grant Itin",
+    17: "Matt Jones",
+    18: "Jack Koutcher",
+    19: "Jack Maloney",
+    20: "Alexander Piasecki",
+    21: "Sam Putter",
+    22: "Jack Rischitelli",
+    23: "Jaidyn Rivera",
+    24: "Charlie Schreiber",
+    25: "Nick Shanahan",
+    26: "Ian Thomas",
+    27: "Ayush Varma",
+    28: "G Word",
+    29: "Kaiser Zhao"
+}
+
+# Data from extraction of Canvas logs:
+
+# 01 Tyler Alday
+	
+# 02 Sean Allen
+
+# 03 Jake Barton
+
+# 04 Adonai Belete
+
+# 05 TJ Brodnik
+
+# 06 Maxx Byrne
+
+# 07 Rauden Clarke
+
+# 08 Brody Collins
+	
+# 09 Anthony Costa
+
+# 10 Gabe Danisi
+
+# 11 Braiden DeLacy
+	
+# 12 Connor Dodson
+
+# Dillon Gamble
+# No Attempt---
+# No Score---
+# No Time---
+# No Log---
+# None	
+
+# 13 Jonah Grunes
+
+# 14 John Hank
+
+# 15 Desmond Heneks
+	
+# Matt Hoban
+# No Attempt---
+# No Score---
+# No Time---
+# No Log---
+# None
+
+# 16 Grant Itin
+
+# 17 Matt Jones
+
+# 18 Jack Koutcher
+	
+# 19 Jack Maloney
+
+# Peter McElhone
+# No Attempt---
+# No Score---
+# No Time---
+# No Log---
+# None
+
+# 20 Alexander Piasecki
+
+# 21 Sam Putter
+
+# 22 Jack Rischitelli
+
+# 23 Jaidyn Rivera
+	
+# 24 Charlie Schreiber
+
+# 25 Nick Shanahan
+
+# 26 Ian Thomas
+
+# 27 Ayush Varma
+
+# 28 G Word
+
+# 29 Kaiser Zhao
